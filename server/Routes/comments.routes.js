b1ef8db19a950455f2426093b8928cef60bac061
joshuatauro <joshuatauro45@gmail.com>
2023-01-21 08:49:57 +0530

    client

---
+++ server/Routes/comments.routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+
+router.post("/publish", async(req, res) => {
+  const { comment, qID, aID } = req.body
+
+  try{
+    
+  } catch(err) {
+
+  }
+
+})
+
+
+module.exports = router
