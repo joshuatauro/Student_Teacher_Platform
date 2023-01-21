835a691afb55bb0ae40f93f17cad1c0467dcdecf
joshuatauro <joshuatauro45@gmail.com>
2023-01-21 09:09:20 +0530

    new

--- server/Routes/comments.routes.js
+++ server/Routes/comments.routes.js
@@ -12,6 +12,7 @@ router.post("/post", async(req, res) => {
     res.status(200).json({code: 1, details: postCommentQuery.rows[0]})
   } catch(err) {
     console.log(err)
+    console.log(err)
   }
 
 })
